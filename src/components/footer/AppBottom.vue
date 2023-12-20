045d6be1650876a9908466deb16062d967d99a09
gaetano-94 <gaetano.panico1994@gmail.com>
2023-12-20 15:14:02 +0100

    feat: footer bottom

--- src/components/footer/AppBottom.vue
+++ src/components/footer/AppBottom.vue
@@ -4,6 +4,28 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <section class="gp-bg">
+    <div class="container d-flex justify-content-between pt-4">
+      <span>Enjoy the low price.We are tracking any intention of piracy.</span>
+      <span
+        ><i class="fa-regular fa-copyright" style="color: #ffffff"></i> 2020
+        NEXGEN is Proudly Powered by <span class="codings">Codings.</span></span
+      >
+    </div>
+  </section>
+</template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.gp-bg {
+  background-color: #0d0d11;
+
+  span {
+    color: white;
+  }
+
+  .codings {
+    color: green;
+  }
+}
+</style>
